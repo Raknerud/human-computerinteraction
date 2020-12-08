744713a1156108518135d716532c887d3f9eb8bc
dustindsampson <45111143+dustindsampson@users.noreply.github.com>
2020-12-08 09:17:25 -0600

    Update MarketNBC.R

--- dataminingproject/R/MarketNBC.R
+++ dataminingproject/R/MarketNBC.R
@@ -5,9 +5,9 @@
 
 setwd("C:/Users/dd-sa/OneDrive/Documents/GitHub/introtodatamining/dataminingproject/R")
 
-trainingFile = "irisTraining.txt"
-testingFile = "irisTesting.txt"
-Xtrain = as.matrix(read.table(trainingFile))
+dataFile = "supermarket.txt"
+
+Xtrain = as.data.frame(read.table(dataFile))
 n = dim(Xtrain)[1]
 d = dim(Xtrain)[2] - 1 # the last attribute is the class label, so it does not count.
 #Training... Collect mean and standard deviation for each dimension for each class..
@@ -15,10 +15,18 @@ d = dim(Xtrain)[2] - 1 # the last attribute is the class label, so it does not c
 idp = which(Xtrain[,d+1] ==1) # points that have 1 as the class label
 np = length(idp)
 Xpositive = Xtrain[idp,1:d]
+avgPositive=colMeans(Xpositive)
+sdp=apply(Xpositive,2,sd)
 
+idn = which(xTrain[,d+1] ==-1)
+pn=length(idn)/n
+xNegative= xTrain[idn,1:d]
+avgNegative=colMeans(xNegative)
+sdn=apply(xNegative,2,sd)
 #Testing .....
-Xtest=as.matrix(read.table(testingFile))
-nn = dim(Xtest)[1] # Number of points in the testing data.
+xTest=as.matrix(read.table(testingFile))
+nn = dim(xTest)[1] # Number of points in the testing data.
+
 
 tp = 0 #True Positive
 fp = 0 #False Positive
@@ -33,6 +41,41 @@ for (i in 1:nn) {
   #P(Xi|C+) = P(Xi1|C+) * P(Xi2|C+)....P(Xid|C+)....Do the same for P(Xi|C-)
   #Now that you've calculate P(Xi|C+) and P(Xi|C-), we can decide which is higher
   #P(Xi|C-)*P(C-) or P(Xi|C-)*P(C-) ..
-  #increment TP,FP,FN,TN accordingly, remember the true lable for the ith point is in Xtest[i,(d+1)]
-
+  #increment TP,FP,FN,TN accordingly, remember the true lable for the ith point is in xTest[i,(d+1)]
+  pv=dnorm(xTest[i,1:d],avgPositive,sdp )
+  PxPos=prod(pv)
+  productPos=prod(PxPos,np)
+  nv=dnorm(xTest[i,1:d],avgNegative,sdn )
+  PxNeg=prod(nv)
+  productNeg=prod(PxNeg,pn)
+  if(productPos>=productNeg){
+    expectVal=1
+  }else{expectVal=-1}
+  if(expectVal==1){
+    if(xTest[i,d+1]==1){
+      tp=tp+1
+    }else{
+      fp=fp+1
+    }
+  }
+  else{
+    if(xTest[i,d+1]==-1){
+      tn=tn+1
+    }else{
+      fn=fn+1
+    }
+  }
 }
+
+
+#Calculate all the measures required..
+Accuracy=(tp+tn)/(tp+fp+tn+fn)
+precision=(tp)/(tp+fp)
+recall=(tp)/(tp+fn)
+testData <- matrix(c(Accuracy, tp, tn, fp, fn, precision, recall),ncol=7, byrow = TRUE)
+colnames(testData)<-c("Accuracy", "TP","TN","FP", "FN", "Precision", "Recall")
+testData <- as.table(testData)
+testData
+
+write.table(testData, file = "testData.txt", sep = "\t",
+            row.names = TRUE, col.names = NA)
